becbebef5358bec06a861ba2dfeca19e4d0b1784
hengxin <hfwei@nju.edu.cn>
2021-07-17 16:23:22 +0800

    ncurses/menu/: +menu-basics, +menu-window

---
+++ cmake-build-debug/ncurses/CMakeFiles/menu-basics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/menu-basics.dir/menus/menu-basics.c.o"
+  "menu-basics"
+  "menu-basics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/menu-basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/ncurses/CMakeFiles/menu-window.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/menu-window.dir/menus/menu-window.c.o"
+  "menu-window"
+  "menu-window.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/menu-window.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
